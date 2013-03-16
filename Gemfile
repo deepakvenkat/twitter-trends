b38498f3c3222f609e5a133296f65a5f5b488ff0
deepakvenkat <dipak2911@gmail.com>
2013-03-16 17:25:58 -0400

    used the new twitter gem and added config to include
    twitter api keys

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "sinatra-contrib"
 
 # The rerun command restarts the app if your files change
 gem "rerun"
--- app.rb
+++ app.rb
@@ -4,13 +4,30 @@ require 'sinatra'
 require 'twitter'
 require 'json'
 
+require "sinatra/config_file"
+
+config_file 'keys.yml'
+
+enable :sessions
 configure do
   set :public_folder, Proc.new { File.join(root, "static") }
 end
 
 helpers do
-
+  def current_user
+    @current_user ||= User.get(session[:user_id]) if session[:user_id]
+  end
 end
+
+before do
+  Twitter.configure do |config|
+    config.consumer_key = settings.twitter["consumer_key"]
+    config.consumer_secret = settings.twitter["consumer_secret"]
+    config.oauth_token = settings.twitter["oauth_token"]
+    config.oauth_token_secret = settings.twitter["oauth_token_secret"]
+  end
+end
+
 
 get '/' do
   erb :root
